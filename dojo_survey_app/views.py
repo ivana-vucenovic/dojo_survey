f8afffe17c465f9494e50250c426074f77c1c29b
Ivana Vucenovic <ivucenovic@hotmail.com>
2021-09-23 11:06:19 -0400

    first commit

---
+++ dojo_survey_app/views.py
@@ -0,0 +1,19 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+
+def process(request):
+    if request.method == 'GET':
+        return render (request, 'result.html')
+    if request.method == 'POST':
+        request.session['first_name']=request.POST['first_name']
+        request.session['city']=request.POST['city']
+        request.session['comment']=request.POST['comment']
+        request.session['leng']=request.POST.getlist('leng')
+        
+
+    return redirect ('/process')
+
+def index(request):
+    return render (request, 'django.html')
+
+
